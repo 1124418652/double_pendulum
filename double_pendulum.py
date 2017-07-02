92b26298e63d6ce4cb2761935d374b27d526d6c4
hejun <112418652@qq.com>
2017-07-02 11:11:11 +0800

    添加计算拉格朗日等式的函数lagrange_equation

--- double_pendulum.py
+++ double_pendulum.py
@@ -5,7 +5,7 @@ from sympy import Derivative as D
 import math
 import numpy as np
 
-var("x1 x2 y1 y2 l1 l2 m1 m2 th_1 th_2 dth_1 dth_2 ddth_1 ddth_2 t g tmp")
+var("x1 x2 y1 y2 l1 l2 m1 m2 th_1 th_2 dth_1 dth_2 ddth_1 ddth_2 t g tmp tmp2")
 
 #the coordinates of the two balls are (x1,y1),(x2,y2)
 x1 = l1 * sin(th_1(t))
@@ -20,9 +20,21 @@ v_y2 = diff(y2, t)
 
 #lagrange value
 L = m1/2 * (v_x1**2 + v_y1**2) + m2/2 * (v_x2**2 + v_y2**2) - m1 * g * y1 - m2 * g * y2
+print("the lagrange value is:\n")
+pprint(simplify(L))
 
 #calculate lagrange equation
 def lagrange_equation(L, th):
+    ddL_th = L.subs(D(th(t),t), tmp).diff(tmp,1).subs(tmp, D(th(t),t))
+    dL_th = L.subs(D(th(t),t), tmp).subs(th(t),tmp2).diff(tmp2,1).subs(tmp2,th(t)).subs(tmp,D(th(t),t))
+    L_equ = ddL_th.diff(t,1) - dL_th
+    print("the lagrange equation is:\n")
+    pprint(simplify(L_equ))
+    return L_equ
+    
+    
+
+
 
 
 
